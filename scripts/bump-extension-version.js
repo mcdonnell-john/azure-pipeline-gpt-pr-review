68f313d513d2dde4939b4e95c3451b87ce1ffc4f
John McDonnell <mcdonnell.john@gmail.com>
2025-04-21 07:59:08 +0100

    chore: fixed file path issues

--- scripts/bump-extension-version.js
+++ scripts/bump-extension-version.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-const ext = JSON.parse(fs.readFileSync('vss-extension.json', 'utf8'));
+const pkg = JSON.parse(fs.readFileSync('../GPTPullRequestReview/package.json', 'utf8'));
+const ext = JSON.parse(fs.readFileSync('../vss-extension.json', 'utf8'));
 
-ext.version = pkg.version;  // get npm package version
+ext.version = pkg.version; 
 
 fs.writeFileSync('vss-extension.json', JSON.stringify(ext, null, 2));
 
