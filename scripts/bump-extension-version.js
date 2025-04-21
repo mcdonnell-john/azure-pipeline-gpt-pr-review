818da014db35a8fc6d076782600a268b2ff31e70
John McDonnell <mcdonnell.john@gmail.com>
2025-04-21 14:40:00 +0100

    chore: fixing file path error

--- scripts/bump-extension-version.js
+++ scripts/bump-extension-version.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
-const path = require('path');
 
 // Load package.json
 const pkg = JSON.parse(fs.readFileSync('./GPTPullRequestReview/package.json', 'utf8'));
 
 // --- Update vss-extension.json ---
 const extPath = 'vss-extension.json';
-const ext = JSON.parse(fs.readFileSync(extPath, 'utf8'));
+const ext = JSON.parse(fs.readFileSync('vss-extension.json', 'utf8'));
 ext.version = pkg.version;
 fs.writeFileSync(extPath, JSON.stringify(ext, null, 2));
 console.log(`✅ vss-extension.json version set to ${pkg.version}`);
 
 // --- Update task.json patch version ---
-const taskPath = path.join(__dirname, 'GPTPullRequestReview', 'task.json');
+const taskPath = JSON.parse(fs.readFileSync('./GPTPullRequestReview/task.json', 'utf8'));
 const task = JSON.parse(fs.readFileSync(taskPath, 'utf8'));
 
 const [major, minor, patch] = task.version;
