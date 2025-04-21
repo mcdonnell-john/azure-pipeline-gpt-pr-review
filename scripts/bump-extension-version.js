6fd8781fbc67b8e64b40f7c8bfc23453536046d4
John McDonnell <mcdonnell.john@gmail.com>
2025-04-21 08:06:19 +0100

    chore: Another change as I forgot where this script is executed from

--- scripts/bump-extension-version.js
+++ scripts/bump-extension-version.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const pkg = JSON.parse(fs.readFileSync('../GPTPullRequestReview/package.json', 'utf8'));
-const ext = JSON.parse(fs.readFileSync('../vss-extension.json', 'utf8'));
+const pkg = JSON.parse(fs.readFileSync('./GPTPullRequestReview/package.json', 'utf8'));
+const ext = JSON.parse(fs.readFileSync('vss-extension.json', 'utf8'));
 
 ext.version = pkg.version; 
 
