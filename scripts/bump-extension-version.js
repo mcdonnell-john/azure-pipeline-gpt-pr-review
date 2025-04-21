cd68a82c351b08161c6dab49b8a61569e2e169ae
John McDonnell <mcdonnell.john@gmail.com>
2025-04-21 20:07:35 +0100

    chore: Added support for a Dev Extetension to support further development

--- scripts/bump-extension-version.js
+++ scripts/bump-extension-version.js
@@ -1,25 +1,34 @@
 const fs = require('fs');
 
+// Function to update version in a JSON file
+function updateVersionInFile(filePath, version) {
+    const fileData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    fileData.version = version;
+    fs.writeFileSync(filePath, JSON.stringify(fileData, null, 2));
+    console.log(`✅ ${filePath} version set to ${version}`);
+}
+
+// Function to update task version
+function bumpTaskPatchVersion(taskPath) {
+    const task = JSON.parse(fs.readFileSync(taskPath, 'utf8'));
+    const { Major, Minor, Patch } = task.version;
+    task.version = {
+        Major,
+        Minor,
+        Patch: String(Number(Patch) + 1)
+    };
+    fs.writeFileSync(taskPath, JSON.stringify(task, null, 2));
+    console.log(`✅ task.json patch version bumped to ${task.version.Major}.${task.version.Minor}.${task.version.Patch}`);
+}
+
 // Load package.json
 const pkg = JSON.parse(fs.readFileSync('./GPTPullRequestReview/package.json', 'utf8'));
 
-// --- Update vss-extension.json ---
-const extPath = 'vss-extension.json';
-const ext = JSON.parse(fs.readFileSync('vss-extension.json', 'utf8'));
-ext.version = pkg.version;
-fs.writeFileSync(extPath, JSON.stringify(ext, null, 2));
-console.log(`✅ vss-extension.json version set to ${pkg.version}`);
+// Update vss-extension.json
+updateVersionInFile('./vss-extension.json', pkg.version);
 
-// --- Update task.json patch version ---
-const taskPath = './GPTPullRequestReview/task.json';
-const task = JSON.parse(fs.readFileSync(taskPath, 'utf8'));
+// Update vss-extension-dev.json
+updateVersionInFile('./vss-extension-dev.json', pkg.version);
 
-const { Major, Minor, Patch } = task.version;
-task.version = {
-    Major,
-    Minor,
-    Patch: String(Number(Patch) + 1)
-};
-
-fs.writeFileSync(taskPath, JSON.stringify(task, null, 2));
-console.log(`✅ task.json patch version bumped to ${task.version.Major}.${task.version.Minor}.${task.version.Patch}`);
+// Bump patch version in task.json
+bumpTaskPatchVersion('./GPTPullRequestReview/task.json');
